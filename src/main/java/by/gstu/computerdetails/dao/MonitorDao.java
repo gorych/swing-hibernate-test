1dbba6c23781a49a66a824dd2240227b09b2e1a2
Yahor_Semianchenia <yahor_semianchenia@epam.com>
2016-07-06 11:40:18 +0300

    Add monitor DAO

---
+++ src/main/java/by/gstu/computerdetails/dao/MonitorDao.java
@@ -0,0 +1,11 @@
+package by.gstu.computerdetails.dao;
+
+import by.gstu.computerdetails.entity.Monitor;
+
+import java.util.List;
+
+public interface MonitorDao {
+
+    List<Monitor> findAll();
+
+}
---
+++ src/main/java/by/gstu/computerdetails/dao/impl/MonitorDaoImpl.java
@@ -0,0 +1,19 @@
+package by.gstu.computerdetails.dao.impl;
+
+import by.gstu.computerdetails.config.HibernateUtil;
+import by.gstu.computerdetails.dao.MonitorDao;
+import by.gstu.computerdetails.entity.Monitor;
+
+import java.util.List;
+
+public class MonitorDaoImpl extends GenericDaoImpl<Monitor, Long> implements MonitorDao {
+
+    @SuppressWarnings("unchecked")
+    public List<Monitor> findAll() {
+        HibernateUtil.beginTransaction();
+        List<Monitor> monitors = getSession().createQuery("from Monitor").list();
+        HibernateUtil.commitTransaction();
+
+        return monitors;
+    }
+}
