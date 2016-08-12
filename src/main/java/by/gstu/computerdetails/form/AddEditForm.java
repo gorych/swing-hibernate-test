37bfb236e737bdff8b64ff0d494eda8f84e4cc0a
Yahor_Semianchenia <yahor_semianchenia@epam.com>
2016-08-12 12:49:29 +0300

    Create add monitor form

---
+++ src/main/java/by/gstu/computerdetails/form/AddEditForm.java
@@ -0,0 +1,60 @@
+package by.gstu.computerdetails.form;
+
+import by.gstu.computerdetails.dao.MonitorDao;
+import by.gstu.computerdetails.dao.ScreenResolutionDao;
+import by.gstu.computerdetails.dao.impl.MonitorDaoImpl;
+import by.gstu.computerdetails.dao.impl.ScreenResolutionDaoImpl;
+import by.gstu.computerdetails.entity.Monitor;
+import by.gstu.computerdetails.entity.ScreenResolution;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class AddEditForm {
+
+    private static final MonitorDao MONITOR_DAO;
+    private static final ScreenResolutionDao SCREEN_RESOLUTION_DAO;
+
+    static {
+        MONITOR_DAO = new MonitorDaoImpl();
+        SCREEN_RESOLUTION_DAO = new ScreenResolutionDaoImpl();
+    }
+
+    private JPanel rootPanel;
+
+    private JTextField nameField;
+    private JTextField diagonalField;
+    private JTextField guaranteeField;
+    private JTextField priceField;
+
+    private JButton saveBtn;
+    private JComboBox<ScreenResolution> resolutionCb;
+
+    public AddEditForm() {
+        List<ScreenResolution> resolutions = SCREEN_RESOLUTION_DAO.findAll();
+        for (ScreenResolution resolution : resolutions) {
+            resolutionCb.addItem(resolution);
+        }
+
+        saveBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String name = nameField.getText();
+                double diagonal = Double.valueOf(diagonalField.getText());
+                int guarantee = Integer.valueOf(guaranteeField.getText());
+                BigDecimal price = BigDecimal.valueOf(Long.valueOf(priceField.getText()));
+                ScreenResolution screenResolution = (ScreenResolution) resolutionCb.getSelectedItem();
+
+                MONITOR_DAO.saveOrUpdate(new Monitor(name, price, diagonal, guarantee, screenResolution));
+            }
+        });
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+
+}
