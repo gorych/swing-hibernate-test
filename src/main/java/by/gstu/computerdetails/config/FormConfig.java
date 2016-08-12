18c729337f8b71ef38bc298b1044f5ae0bcaf5c2
Yahor_Semianchenia <yahor_semianchenia@epam.com>
2016-08-12 12:50:44 +0300

    Add constants for add form

--- src/main/java/by/gstu/computerdetails/config/FormConfig.java
+++ src/main/java/by/gstu/computerdetails/config/FormConfig.java
@@ -3,8 +3,13 @@ package by.gstu.computerdetails.config;
 public final class FormConfig {
 
     public static final String APP_NAME = "Система поддержки принятия решений в области покупки компьютерных мониторов";
-    public static final int DEFAULT_WINDOW_WIDTH = 700;
+    public static final String ADD_MONITOR_FORM_NAME = "Добавление нового монитора";
+
+    public static final int DEFAULT_WINDOW_WIDTH = 750;
     public static final int DEFAULT_WINDOW_HEIGHT = 450;
+
+    public static final int ADD_FORM_DEFAULT_WIDTH = 350;
+    public static final int ADD_FORM_DEFAULT_HEIGHT = 280;
 
     private FormConfig() {
     }
